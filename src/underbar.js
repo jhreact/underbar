8b78097a9b77580128cf3e0fd34348c4ff3b0e0f
jhreact <jhreact (at) users.noreply.github.com>
2014-05-16 13:07:56 -0700

    delay passing all tests

--- src/underbar.js
+++ src/underbar.js
@@ -302,6 +302,10 @@ var _ = {};
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = Array.prototype.slice.call(arguments);
+    return setTimeout(function(){
+      return func.apply(this, args.slice(2));
+    }, wait);
   };
 
 
