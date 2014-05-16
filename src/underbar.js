fa2d06d9ed87ec30c897111818580cf085f252d3
jhreact <jhreact (at) users.noreply.github.com>
2014-05-15 23:54:38 -0700

    some: all tests passing

--- src/underbar.js
+++ src/underbar.js
@@ -194,6 +194,10 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    // TODO: Revisit this using every()
+    return _.reduce(collection, function(isTrue, item) {
+      return isTrue || Boolean(iterator ? iterator(item) : item);
+    }, false);
   };
 
 
